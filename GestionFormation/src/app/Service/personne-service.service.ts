f784d0379c22a9a42bad23e5ea2793dc79aca966
avallee <antoine-vallee@live.fr>
2022-12-15 16:51:02 +0100

    ajout des commandes pour les services de RDV, Historique, Personnes Antoine

--- GestionFormation/src/app/Service/personne-service.service.ts
+++ GestionFormation/src/app/Service/personne-service.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Personne } from '../Model/personne.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonneServiceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getAll(){
+    return this.http.get<Personne[]>('http://localhost:8020/api/personnes')
+  }
+
+  getbyId(id:number){
+    return this.http.get<Personne>(`http://localhost:8020/api/personnes/${id}`)
+  }
+
+  getbyName(nom:string){
+    return this.http.get<Personne[]>(`http://localhost:8020/api/personnes/chercher/${nom}`)
+  }
+
+  post(personne:Personne){
+    return this.http.put<Personne>('http://localhost:8020/api/personnes',personne)
+  }
+
+
+  delete(id:number){
+    return this.http.delete<Personne>(`http://localhost:8020/api/personnes/${id}`)
+  }
+
+  contact(data : FormData){
+    return this.http.post<Personne>('http://localhost:8020/api/personnes', data)
+  }
+
 }
--- GestionFormation/src/app/Service/rdvservice.service.ts
+++ GestionFormation/src/app/Service/rdvservice.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RDV } from '../Model/rdv.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RDVServiceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getAll(){
+    return this.http.get<RDV[]>('http://localhost:8020/api/rdvs')
+  }
+
+  getbyId(id:number){
+    return this.http.get<RDV>(`http://localhost:8020/api/rdvs/${id}`)
+  }
+
+  post(rdv:RDV){
+    return this.http.put<RDV>('http://localhost:8020/api/rdvs',rdv)
+  }
+
+
+  delete(id:number){
+    return this.http.delete<RDV>(`http://localhost:8020/api/rdvs/${id}`)
+  }
+
+  
 }
