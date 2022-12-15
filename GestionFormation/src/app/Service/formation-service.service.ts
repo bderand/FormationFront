93e9064291584c490950a446c1b3970de4a20446
Bastien <bastienderand@hotmail.fr>
2022-12-15 17:38:30 +0100

    Ajout Formateur et formation service

--- GestionFormation/src/app/Service/formation-service.service.ts
+++ GestionFormation/src/app/Service/formation-service.service.ts
@@ -1,9 +1,47 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Formateur } from '../Model/formateur.model';
+import { Formation } from '../Model/formation.model';
+import { Participant } from '../Model/participant.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormationServiceService {
 
-  constructor() { }
+  url:string = "http://localhost:8020/api/Formations"
+
+  constructor(private http:HttpClient) { }
+
+
+  public getFormations_all(){
+
+    this.http.get<Formation[]>(this.url);
+  }
+
+  public getFormations_id(id:number){
+
+    this.http.get<Formation>(this.url + `/${id}`);
+  }
+
+  public getFormation_participants(id:number){
+
+    this.http.get<Participant[]>(this.url + `/${id}/participants`);
+  }
+
+  public addFormateur(data:FormData){
+
+    this.http.post<Formateur>(this.url + "/formateur",data);
+  }
+
+  public addFormation(formation:Formation){
+
+    this.http.post(this.url, formation);
+  }
+
+  public suppFormation(id:number){
+
+    this.http.delete(this.url + `/${id}`);
+  }
+
 }
