957c21240fe4344ee474ab6b917b3106f1a82151
avallee <antoine-vallee@live.fr>
2022-12-16 11:31:14 +0100

    path guard interceptor

--- GestionFormation/src/app/Guard/guard.guard.ts
+++ GestionFormation/src/app/Guard/guard.guard.ts
@@ -1,15 +1,40 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot, CanActivate, Route, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GuardGuard implements CanActivate {
+  constructor(private router : Router) { } 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
-  
+      if (this.isLoggedIn(route)) {      
+        return true;      
+        }      
+        // navigate to login page as user is not authenticated      
+        this.router.navigate(['/connexion']);      
+         return false;      
+  }      
+  public isLoggedIn(r:ActivatedRouteSnapshot): boolean {      
+     let status = false;
+     let session = sessionStorage.getItem('user');
+     let role = r.data['role'];    
+     if (session) {      
+        status = true;
+        if(role == undefined)return true;   
+        let chaine = sessionStorage.getItem('user') ?? "";
+        let user = JSON.parse(chaine);
+        if(r.data['role'] == user.role.nom)
+          {
+            return true;
+          } else return false;   
+     } else {      
+        status = false;      
+        }      
+     return status;      
+  } 
 }
+  
+
--- GestionFormation/src/app/app-routing.module.ts
+++ GestionFormation/src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConnexionComponent } from './Component/connexion/connexion.component';
+import { FormateurComponentComponent } from './Component/formateur-component/formateur-component.component';
+import { FormationComponentComponent } from './Component/formation-component/formation-component.component';
+import { HistoriqueComponentComponent } from './Component/historique-component/historique-component.component';
+import { InscriptionComponent } from './Component/inscription/inscription.component';
+import { PaiementComponentComponent } from './Component/paiement-component/paiement-component.component';
+import { ParticipantComponentComponent } from './Component/participant-component/participant-component.component';
+import { PersonneComponentComponent } from './Component/personne-component/personne-component.component';
+import { RDVComponentComponent } from './Component/rdvcomponent/rdvcomponent.component';
+import { UtilisateurComponentComponent } from './Component/utilisateur-component/utilisateur-component.component';
+import { GuardGuard } from './Guard/guard.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'connexion', component:ConnexionComponent},
+  {path:'commercial/inscription', component:InscriptionComponent, canActivate : [GuardGuard], data : {role : "commercial"}},
+  {path:'commercial/personnne', component:PersonneComponentComponent, canActivate : [GuardGuard], data : {role : "commercial"}},
+  {path:'formateur/perso', component:FormateurComponentComponent, canActivate : [GuardGuard], data : {role : "formateur"}},
+  {path:'formation', component:FormationComponentComponent, canActivate : [GuardGuard]},
+  {path:'commercial/historique', component:HistoriqueComponentComponent, canActivate : [GuardGuard], data : {role : "commercial"}},
+  {path:'participant/paiement', component:PaiementComponentComponent, canActivate : [GuardGuard], data : {role : "participant"}},
+  {path:'participant/perso', component:ParticipantComponentComponent, canActivate : [GuardGuard], data : {role : "participant"}},
+  {path:'commercial/rdv', component:RDVComponentComponent, canActivate : [GuardGuard], data : {role : "commercial"}},
+  {path:'admin/utilisateur', component:UtilisateurComponentComponent, canActivate : [GuardGuard], data : {role : "admin"}}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
