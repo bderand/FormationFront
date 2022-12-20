e635db171b4e979039284e36516e411d9ef3a906
Bastien <bastienderand@hotmail.fr>
2022-12-20 18:55:59 +0100

    Gestion des paiements et relance des paiements du côté de l'assistant

--- GestionFormation/src/app/Service/participant-service.service.ts
+++ GestionFormation/src/app/Service/participant-service.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Formation } from '../Model/formation.model';
 import { Participant } from '../Model/participant.model';
 
 @Injectable({
@@ -11,5 +12,23 @@ export class ParticipantServiceService {
 
   postparticipant(data :  FormData){
     return this.http.post<Participant>('http://localhost:8020/api/participants/nouveau',data);
+  }
+
+  getFormations_participantsID(id:number){
+    return this.http.get<Formation[]>(`http://localhost:8020/api/participants/${id}/formations`);
+  }
+
+  getParticipants_all(){
+
+    return this.http.get<Participant[]>('http://localhost:8020/api/participants');
+  }
+
+  getParticipant_id(id:number){
+
+    return this.http.get<Participant>(`http://localhost:8020/api/participants/${id}`)
+  }
+
+  demanderPaiement(data:FormData){
+    return this.http.post("http://localhost:8020/api/participants/paiement",data);
   }
 }
